daeb929afcfcb29698b9b3b6be1ee43e1a4cc5ec
Asif R Choudhury <choudhury.asif@gmail.com>
2015-10-11 21:52:26 -0400

    Wire up front end to create new listing

--- routes/listings.js
+++ routes/listings.js
@@ -1,8 +1,8 @@
 var router = require('express').Router();
 
 var DB = require('../models/db');
-var User = require('../models/user')(DB.mongoose);
-var Listing = require('../models/listing')(DB.mongoose);
+var User = require('../models/User')(DB.mongoose);
+var Listing = require('../models/Listing')(DB.mongoose);
 
 var respond = function(res, result) {
   res.contentType('application/json');
@@ -10,7 +10,15 @@ var respond = function(res, result) {
 };
 
 router.post('/', function(req, res) {
-  respond(res, { bod: req.body.location});
+  var user = req.session.user || {};
+  var listingData = {
+    'user': user._id,
+    'location': req.body.location,
+    'description': req.body.description
+  };
+  Listing.create(listingData, function(resp) {
+    respond(res, resp);
+  });
 });
 
 module.exports = router;
