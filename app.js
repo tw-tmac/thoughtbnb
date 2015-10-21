fce1155419b9f54f9e633f30e277a011387ac2ef
jessicamann <jmann1163@gmail.com>
2015-10-20 23:44:58 -0400

    Fixed bug: landing on not found page after logging in first time. Added favicon

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(expressSession({
 }));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
