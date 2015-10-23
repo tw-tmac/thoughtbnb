f6414ba5de6e2baa6a776ffebaecec8ede1d578c
Asif R Choudhury <choudhury.asif@gmail.com>
2015-10-23 00:20:54 -0400

    Moved most of userHome.js Listing logic into ListingsController.js/Angular

--- public/scripts/listingsController.js
+++ public/scripts/listingsController.js
@@ -1,4 +1,36 @@
 var app = angular.module('thoughtbnb', []);
-app.controller('ListingsController', function($scope) {
+app.controller('ListingsController', function($scope, $http) {
+  var controller = this;
+
   $scope.listings = [];
+  $scope.formError = "";
+
+  controller.getAll = function() {
+    $http.get('/api/listings').success(function(response) {
+     $scope.listings = response.data.listings;
+    });
+  };
+
+  controller.resetForm = function() {
+    $scope.formError = "";
+    controller.newLocation = "";
+    controller.newDescription = "";
+  };
+
+  controller.add = function() {
+    var listingData = {
+      location: controller.newLocation,
+      description: controller.newDescription
+    };
+    var request = $http.post('/api/listings', listingData).then(function(response) {
+      if (response.data.error) {
+        $scope.formError = response.data.error;
+        return false;
+      }
+      controller.getAll();
+      controller.resetForm();
+    });
+  };
+
+
 });
--- public/scripts/userHome.js
+++ public/scripts/userHome.js
@@ -1,40 +1,4 @@
-var addAddress = function() {
-  var listing = {
-    'location': $('#location').val(),
-    'description': $('#description').val()
-  };
-  $.post('/api/listings/', listing, function(data) {
-    if (data.error) {
-      $('#formError').text(data.error);
-      return false;
-    }
-    updateListings();
-    resetListingForm();
-  });
-};
-
-var resetListingForm = function() {
-  $('#location, #description').val('');
-  $('#formError').text('');
-
-};
-
-var updateListings = function() {
-  $.get('/api/listings/me', function(resp) {
-
-    var scope = angular.element($('#listings')).scope();
-    scope.$apply(function() {
-      scope.listings = resp.data.listings;
-    });
-  });
-};
-
 var initLocationSearch = function() {
   var input = document.getElementById('location');
   var autocomplete = new google.maps.places.Autocomplete(input);
 };
-
-$(function() {
-  $('#btnAdd').on('click', addAddress);
-  updateListings();
-});
